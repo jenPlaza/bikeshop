cd28e9f04bc0239d1ecc6384e007c63e5be4507c
jenPlaza <39809852+jenPlaza@users.noreply.github.com>
2019-09-15 10:43:33 -0400

    Linked json to content.js file, and added articles level 1, 2. I also changed body img, and reformated the header.

--- hikers/setup_hikersguide/content.js
+++ hikers/setup_hikersguide/content.js
@@ -3,8 +3,74 @@
  * https://joshbloom.github.io/dws1/data/hikersguide.json
 */
 
-var xhr = new XMLHttpRequest();
-    xhr.onload = fuction(){
+//testing
+//alert("hello");
+var myBlog = new XMLHttpRequest();
+myBlog.open('GET', 'https://joshbloom.github.io/dws1/data/hikersguide.json');
 
+let htmlBlog = document.getElementById("id01");
+htmlBlog.innerHTML = "Company A";
 
+myBlog.onload = function () {
+
+    var jsonBlog = JSON.parse(myBlog.responseText);
+    //Testing
+    //console.log(jsonData.posts[0]);//works
+    //console.log(jsonData.posts[0].title);//works
+
+    divALevel1(jsonBlog);
+};
+
+function divALevel1(data) {
+
+    //Testing
+    // console.log(data.posts.length);//works
+
+    for (var i = 0; i < data.locations.length; i++) {
+        htmlBlog += '<article>';
+        htmlBlog += '<h3>' + data.locations[i].title + '</h3>';
+        htmlBlog += '<p>' + data.locations[i].text + '</p>';
+        htmlBlog += '</article>';
+    }
+
+    var articleBlog = document.getElementById("aLevel1");
+    articleBlog.innerHTML = htmlBlog;
 }
+myBlog.send();
+
+
+var myAtricle2 = new XMLHttpRequest();
+myAtricle2.open('GET', 'https://joshbloom.github.io/dws1/data/hikersguide.json');
+
+myAtricle2.onload = function () {
+    var jsonArticle2 = JSON.parse(myAtricle2.responseText);
+    divALevel2(jsonArticle2);
+};
+
+function divALevel2(data) {
+    let htmlArticle2 = '';
+
+    //Testing
+   // console.log(data.posts.length);//works
+
+   for (var i = 0; i < data.posts.length; i++) {
+       htmlArticle2 += '<article>';
+       htmlArticle2 += '<img src="' + data.posts[i].imageURL + '"';
+       htmlArticle2 += 'alt="' + data.posts[i].subtitle + '"/>';
+        
+       htmlArticle2 += '<p>' + data.posts[i].subtitle + '</p>';
+
+       htmlArticle2 += '<h3>' + data.posts[i].title + '</h3>';
+
+       htmlArticle2 += '<dl>';
+       htmlArticle2 += '<dt>by ' + data.posts[i].author + '</dt>';
+       htmlArticle2 += '<dt>' + data.posts[i].postDate + '</dt>';
+       htmlArticle2 += '<dt>' + data.posts[i].moreLink + '</dt>';
+       htmlArticle2 += '</dl>';
+       htmlArticle2 += '</article>';
+    }
+
+    var articleBlog = document.getElementById("aLevel2");
+    articleBlog.innerHTML = htmlArticle2;
+}
+myAtricle2.send();
--- hikers/setup_hikersguide/events.js
+++ hikers/setup_hikersguide/events.js
@@ -1,6 +1,46 @@
 /*
- * events.js
- * 
+ * Use AJAX to load the JSON and manipulate the HTML
+ * https://joshbloom.github.io/dws1/data/hikersguide.json
 */
 
-document.write("World");
+//testing
+//alert("hello");
+var myEvents = new XMLHttpRequest();
+myEvents.open('GET', 'https://joshbloom.github.io/dws1/data/hikersguide.json');
+
+let htmlEvents = document.getElementById("id01");
+htmlEvents.innerHTML = "Company A";
+
+window.onload = function () {
+
+    var jsonEvents = JSON.parse(myEvents.responseText);
+
+    //Testing
+    //console.log(jsonData.events[0]);//works
+    //console.log(jsonData.events[0].title);//works
+
+    divALevel3(jsonEvents);
+
+};
+
+function divALevel3(dataEvents) {
+    //Testing
+    //console.log(data.events.length);//works
+
+    for (var i = 0; i < data.events.length; i++) {
+        htmlEvents += '<article>';
+        htmlEvents += '<img src="' + data.events[i].imageURL + '"';
+        htmlEvents += 'alt="' + data.events[i].sponsor + '"/>';
+
+        htmlEvents += '<p><strong>' + data.events[i].subtitle + '</strong></p>';
+
+        htmlEvents += '<h3>' + data.events[i].title + '</h3>';
+        htmlEvents += '<p>' + data.events[i].text + '</p>';
+
+        htmlContent += '</article>';
+    }
+
+    var articleEvents = document.getElementById("events");
+    articleEvents.innerHTML = htmlEvents;
+}
+myEvents.send();
