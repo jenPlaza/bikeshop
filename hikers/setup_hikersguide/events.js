8048dce053bcc8127f572c9cdbfa614314327a7f
jenPlaza <39809852+jenPlaza@users.noreply.github.com>
2019-09-22 14:39:55 -0400

    html, json, and js are working for events, misc, and footer. working on tab menu for the evnt section.

--- hikers/setup_hikersguide/events.js
+++ hikers/setup_hikersguide/events.js
@@ -20,9 +20,18 @@ myEvents.onload = function () {
     secEvents2(jsonEvents);
 };
 
+
+
 function secRotate(dataEvents) {
+
+
     htmlEvents += '<section>';
     htmlEvents += '<img src="HikersGuide/design/File_001 (11).png" alt="Alternate Text" />';
+
+    //var rotator = document.getElementById("id02");
+    //htmlEvents = '<h2>' + dataEvents.events[4].date + '</h2>';
+    //rotator.innerHTML = htmlEvents;
+
     htmlEvents += '<h2>' + dataEvents.events[4].date + '</h2>';
     htmlEvents += '<h3>' + dataEvents.events[4].location + ' </h3>';
 
@@ -47,29 +56,43 @@ function secEvents2(dataEvents2) {
 
     let htmlEvents2 = "";
 
-    var imageArray = ["/HikersGuide/design/sunnytrees.png", "/HikersGuide/design/smokyriver.png", "/HikersGuide/design/tree.png",
-        "/HikersGuide/design/crystalriver.png", "/HikersGuide/design/photo-1507041957456-9c397ce39c97.jpg"];
+    var imageArray = ["/HikersGuide/design/sunnytrees.png", "/HikersGuide/design/File_001 (17).png", "/HikersGuide/design/File_001 (16).png",
+        "/HikersGuide/design/crystalriver.png", "/HikersGuide/design/photo-1507041957456-9c397ce39c97.jpg", "/HikersGuide/design/File_001 (13).png"];
     htmlEvents2 += '<section>';
-    htmlEvents2 += '<dl>';
-    htmlEvents2 += '<dt><strong><a href="#">Today</a></strong></dt>';
-    htmlEvents2 += '<dd></dd>';
-    htmlEvents2 += '<dt><strong><a href="#">Weekend</a></strong></dt>';
-    htmlEvents2 += '<dd></dd>';
-    htmlEvents2 += '<dt><strong><a href="#">This Month</a></strong></dt>';
-    htmlEvents2 += '<dd></dd>';
-    htmlEvents2 += '</dl>';
-    htmlEvents2 += '<br /> <br /> <br />';
-    htmlEvents2 += '<h2>Today</h2>';
+    //htmlEvents2 += '<dl>';
+    //htmlEvents2 += '<dt><strong><a href="#">Today</a></strong></dt>';
+    //htmlEvents2 += '<dd></dd>';
+    //htmlEvents2 += '<dt><strong><a href="#">Weekend</a></strong></dt>';
+    //htmlEvents2 += '<dd></dd>';
+    //htmlEvents2 += '<dt><strong><a href="#">This Month</a></strong></dt>';
+    //htmlEvents2 += '<dd></dd>';
+    //htmlEvents2 += '</dl>';
+    //htmlEvents2 += '<br /><br /><br /><br />';
 
     for (var i = 0; i < dataEvents2.events.length; i++) {
         htmlEvents2 += '<article>';
         htmlEvents2 += '<img src="' + imageArray[i] + '"';
         htmlEvents2 += 'alt="' + dataEvents2.events[i].subtitle + '"/>';
-        htmlEvents2 += '<h3><strong>' + dataEvents2.events[i].title + '</strong></h3>';
-        htmlEvents2 += '<h4>' + dataEvents2.events[i].date + '</h4>';
-        htmlEvents2 += '<p>' + dataEvents2.events[i].location + '</p>';
+        htmlEvents2 += '<section>';
+        htmlEvents2 += '<h3>' + dataEvents2.events[i].title + '<h3>';
+        htmlEvents2 += '<p>' + dataEvents2.events[i].date + '<p>';
+        htmlEvents2 += '<p><strong>' + dataEvents2.events[i].location + '<br/ >';
+        htmlEvents2 += dataEvents2.events[i].state + '</strong></p>';
+        htmlEvents2 += '</section>';
         htmlEvents2 += '</article>';
     }
+
+    htmlEvents2 += '<article>';
+    htmlEvents2 += '<img src="' + imageArray[5] + '"';
+    htmlEvents2 += 'alt="' + dataEvents2.events[3].subtitle + '"/>';
+    htmlEvents2 += '<section>';
+    htmlEvents2 += '<h3>' + dataEvents2.events[3].title + '</h3>';
+    htmlEvents2 += '<p>' + dataEvents2.events[3].date + '</p>';
+    htmlEvents2 += '<p><strong>' + dataEvents2.events[3].location + '<br/ >';
+    htmlEvents2 += dataEvents2.events[3].state + '</strong></p>';
+    htmlEvents2 += '</section>';
+    htmlEvents2 += '</article>';
+
     htmlEvents2 += '</section>';
     var articleEvents = document.getElementById("events");
     articleEvents.innerHTML = htmlEvents2;
