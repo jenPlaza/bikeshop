d21b2d4208f1cc45e7bbfe6e6922c82695fb3b29
jenPlaza <39809852+jenPlaza@users.noreply.github.com>
2019-11-22 16:56:28 -0500

    new file

---
+++ data.js
@@ -0,0 +1,23 @@
+/**
+ * Created by scottwainman on 10/20/16.
+ */
+
+var data = {
+    movies: [
+        {
+            title: "Star Wars",
+            duration: 127,
+            rating: "PG"
+        },
+        {
+            title: "Matrix",
+            duration: 207,
+            rating: "PG-13"
+        },
+        {
+            title: "Aliens",
+            duration: 86,
+            rating: "R"
+        }]
+
+};
